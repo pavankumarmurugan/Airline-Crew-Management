afdf300fdc7d958f9608fc4b474b26b785a8e97a
Pavan Kumar Murugan <pavankumarmurugan@Pavans-MacBook-Air.local>
2024-01-11 22:27:45 +0000

    Commiting the setup

---
+++ AirlineManagement/src/main/java/com/airline/management/AirlineManagementApplication.java
@@ -0,0 +1,13 @@
+package com.airline.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineManagementApplication.class, args);
+	}
+
+}
