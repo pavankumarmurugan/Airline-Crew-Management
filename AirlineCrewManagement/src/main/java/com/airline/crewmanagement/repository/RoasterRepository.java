531835f99e047be981246afca7ac9b108212a50b
Pavan Kumar Murugan <pavankumarmurugan@Pavans-MacBook-Air.local>
2024-02-08 23:19:25 +0000

    Changes for staff assignment

--- AirlineCrewManagement/src/main/java/com/airline/crewmanagement/repository/RoasterRepository.java
+++ AirlineCrewManagement/src/main/java/com/airline/crewmanagement/repository/RoasterRepository.java
@@ -1,11 +1,13 @@
 package com.airline.crewmanagement.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.airline.crewmanagement.entity.FlightEntity;
 import com.airline.crewmanagement.entity.RoasterEntity;
 
 @Repository
@@ -13,5 +15,7 @@ import com.airline.crewmanagement.entity.RoasterEntity;
 public interface RoasterRepository extends JpaRepository<RoasterEntity, Long> {
 	
 	List<RoasterEntity> findByRoasterTripStatusAndFlightOperatingDayOrderByRoasterIdAsc(String roasterTripStatus, String flightOperatingDay);
+	
+	Optional<RoasterEntity> findByRoasterTripStatusAndFlightOperatingDayAndFlightId(String roasterTripStatus,String flightOperatingDay,FlightEntity flightEntity);
 
 }
